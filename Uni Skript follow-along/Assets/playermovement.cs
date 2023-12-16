15ac9dc92a77d5ddb4ca3ced49714c3e43c81be7
Platiai <alenaendlicher@gmail.com>
2023-12-16 13:34:04 +0100

    ayooo

---
+++ Uni Skript follow-along/Assets/playermovement.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class playermovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+
+
+    public float movementspeed = 10;
+    void Update()
+    {
+        
+      if (Input.GetKey(KeyCode.W))
+      {
+        gameObject.transform.position += new Vector3
+            (0, 0.01f, 0) * movementspeed * Time.deltaTime;
+      }  
+      if (Input.GetKey(KeyCode.A))
+      {
+        gameObject.transform.position += new Vector3
+            (-0.01f, 0, 0)* movementspeed * Time.deltaTime;
+      }
+       if (Input.GetKey(KeyCode.S))
+      {
+        gameObject.transform.position += new Vector3
+            (0, -0.01f, 0)* movementspeed * Time.deltaTime;
+      }
+       if (Input.GetKey(KeyCode.D))
+      {
+        gameObject.transform.position += new Vector3
+            (0.01f, 0, 0)* movementspeed * Time.deltaTime;
+      }
+
+    }
+}
